67945b38569c98c1d28fa0549a02f12cae017794
Serhey325 <sabserj2017@gmail.com>
2023-09-16 18:20:14 +0200

    client FIX chatUI

--- client/src/scenes/widgets/ChatWidget.tsx
+++ client/src/scenes/widgets/ChatWidget.tsx
@@ -8,7 +8,7 @@ const ChatWidget = () => {
 
     return (
         <div
-            className={`flex justify-center flex-col pt-0 pb-3 rounded-xl bg-bkg-alt transition duration-300 max-h-[80vh]`}>
+            className={`flex justify-center flex-col pt-0 pb-3 rounded-xl bg-bkg-alt transition duration-300 ${chat ? "max-h-[80vh]" : "min-h-[80vh]"}`}>
             {chat
                 ?
                 <SingleChat chat={chat}/>
